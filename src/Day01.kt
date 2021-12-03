e7d1085200af2665cd9ca5e2a0b6645348679374
John Chiu <john.chiu@workday.com>
2021-12-03 01:01:56 -0800

    Add toInts() for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Int>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Int>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01_test").toInts()
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").toInts()
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,11 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Converts `List<String>` to `List<Int>`
+ */
+fun List<String>.toInts() = map { it.toInt() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
