a89b8fa6bca38cad167459397c40948ab32f9b8f
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-11-29 21:17:50 +0100

    Utils.kt code formatting

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,9 +5,12 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) = File("src", "$name.txt")
+    .readLines()
 
 /**
  * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16).padStart(32, '0')
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
