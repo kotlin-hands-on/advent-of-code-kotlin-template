f18c282fe848e1d0d4a5fccefe8627d16a27159b
Michael Böiers <mikeenregalia@gmail.com>
2022-11-29 20:57:11 +0100

    pad md5 string with zeroes
    2016/day-17 breaks without this, for example

--- src/Utils.kt
+++ src/Utils.kt
@@ -10,4 +10,4 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
 /**
  * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16).padStart(32, '0')
