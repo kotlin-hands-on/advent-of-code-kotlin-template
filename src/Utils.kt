6bb6f6e02d576e0ac5568abe86b82374631468b8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-12-09 11:26:33 +0100

    cleanup

--- src/Utils.kt
+++ src/Utils.kt
@@ -16,8 +16,6 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .padStart(32, '0')
 
 /**
- * The easy way to print anything
+ * The cleaner shorthand for printing output.
  */
-fun Any?.println() {
-    println(this)
-}
+fun Any?.println() = println(this)
