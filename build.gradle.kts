000e02b5aa90e245de54695b75a23f4e74d1fb1e
Armand Mégrot <armand.megrot@gmail.com>
2022-12-11 01:43:45 +0100

    Add gradle task to generate next day
    I'm a little bit lazy and I don't want to copy paste + edit the code of the previous day myself so I wrote this simple gradle task 😅 
    Thought we might want to integrate it to the template repo !

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,4 +16,37 @@ tasks {
     wrapper {
         gradleVersion = "7.6"
     }
+
+    task("generateNextDay") {
+        doLast {
+            val prevDayNum = fileTree("$projectDir/src").matching {
+                include("Day*.kt")
+            }.maxOf {
+                val (prevDayNum) = Regex("Day(\\d\\d)").find(it.name)!!.destructured
+                prevDayNum.toInt()
+            }
+            val newDayNum = String.format("%02d", prevDayNum + 1)
+            File("$projectDir/src", "Day$newDayNum.kt").writeText(
+                """
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("resources/Day01_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("resources/Day01")
+    println(part1(input))
+    println(part2(input))
+}
+"""
+            )
+        }
+    }
 }
