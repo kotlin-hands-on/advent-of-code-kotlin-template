06c5b3838f449fbd01f8424f2588e2fef21a322c
Armand Mégrot <armand.megrot@gmail.com>
2022-12-11 16:16:35 +0100

    Fix day num of input files

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,10 +38,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("resources/Day01_test")
+    val testInput = readInput("resources/Day${newDayNum}_test")
     check(part1(testInput) == 0)
 
-    val input = readInput("resources/Day01")
+    val input = readInput("resources/Day$newDayNum")
     println(part1(input))
     println(part2(input))
 }
