57f0de61cb331f09ba83914bd015fc15c29ce677
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-11-21 09:29:09 +0100

    Use `Path` instead of `File`

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,12 @@
-import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readInput(name: String) = Path("src/$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
