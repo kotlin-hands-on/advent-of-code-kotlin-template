5d04ae324c985aa67bf266c779a041a93059b316
vikrant21st <vikrant21st>
2023-12-19 17:27:03 +0530

    final solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,62 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val day1Part1Input = """
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        """.trimIndent().lines()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val day1Part2Input = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent().lines()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(day1Part1Input) == 142)
+    check(part2(day1Part2Input) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("inputDay1")
     part1(input).println()
     part2(input).println()
+}
+
+fun part2(input: List<String>): Int {
+    val digits = (0..9).map { it.toString() } +
+            listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun Int.indexToExpectedDigit() =
+        takeIf { it != -1 && (it % 10) != 0 }
+            ?.let { it % 10 }
+
+    fun String.firstDigit(): Int {
+        for (partialStr in windowedSequence(5, partialWindows = true)) {
+            return digits.indexOfFirst { partialStr.startsWith(it) }.indexToExpectedDigit() ?: continue
+        }
+        return 0
+    }
+
+    fun String.lastDigit(): Int {
+        for (partialStr in reversedWindowedSequence(5, partialWindows = true)) {
+            return digits.indexOfFirst { partialStr.endsWith(it) }.indexToExpectedDigit() ?: continue
+        }
+        return 0
+    }
+
+    return input.sumOf { (it.firstDigit() * 10) + it.lastDigit() }
+}
+
+fun part1(input: List<String>): Int {
+    fun String.firstDigit(): Int =
+        find { it.isDigit() }?.digitToInt() ?: 0
+
+    fun String.lastDigit(): Int =
+        findLast { it.isDigit() }?.digitToInt() ?: 0
+
+    return input.sumOf { line ->
+        (line.firstDigit() * 10) + line.lastDigit()
+    }
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +19,30 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun CharSequence.reversedWindowedSequence(size: Int, step: Int = 1, partialWindows: Boolean = false): Sequence<String> =
+    reversedWindowedSequence(size, step, partialWindows) { it.toString() }
+
+fun <R> CharSequence.reversedWindowedSequence(
+    size: Int,
+    step: Int = 1,
+    partialWindows: Boolean = false,
+    transform: (CharSequence) -> R,
+): Sequence<R> {
+    checkWindowSizeStep(size, step)
+    val windows = (if (partialWindows) indices else 0 until length - size + 1).reversed() step step
+    return windows.asSequence().map { it + 1 }.map { endIndex ->
+        val start = endIndex - size
+        val coercedStart = if (start < 0) 0 else start
+        transform(subSequence(coercedStart, endIndex))
+    }
+}
+
+private fun checkWindowSizeStep(size: Int, step: Int) {
+    require(size > 0 && step > 0) {
+        if (size != step)
+            "Both size $size and step $step must be greater than zero."
+        else
+            "size $size must be greater than zero."
+    }
+}
