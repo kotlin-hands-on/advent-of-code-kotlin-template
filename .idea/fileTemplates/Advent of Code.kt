a193b6923c31f0c7b700ad3c754699aacc4e6574
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-11-19 16:15:06 +0100

    Add `Advent of Code` file template

---
+++ .idea/fileTemplates/Advent of Code.kt
@@ -0,0 +1,15 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    check(part1(listOf("...")) == 1)
+
+    val input = readInput("Day$Day")
+    part1(input).println()
+    part2(input).println()
+}
