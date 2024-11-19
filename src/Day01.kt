5879ee88a07f33c9829459c3290a0910048d3952
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-11-19 15:54:04 +0100

    Refresh the main Day01.kt file

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,10 +7,14 @@ fun main() {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
+    // Test if implementation meets criteria from the description, like:
+    check(part1(listOf("test_input")) == 1)
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
+    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,12 @@
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
-import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 
 /**
  * Converts string to md5 hash.
