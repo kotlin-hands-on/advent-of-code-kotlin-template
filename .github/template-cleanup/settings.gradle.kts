42ced76f153e1eeeb773d59862481a44b005a471
Alex Resiga <resiga.alex@gmail.com>
2024-11-30 19:43:50 +0200

    Update Amper version to 0.5.0

--- .github/template-cleanup/settings.gradle.kts
+++ .github/template-cleanup/settings.gradle.kts
@@ -1,7 +1,14 @@
 rootProject.name = "%NAME%"
 
-dependencyResolutionManagement {
+pluginManagement {
     repositories {
         mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
     }
+}
+
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
 }
