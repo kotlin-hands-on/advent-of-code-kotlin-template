36edde7426968ce3e604f364892390b100a70c4f
juntingMa <junting.ma.e@thalesdigital.io>
2024-12-04 14:45:26 +0800

    chore: finish up to day 3

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,5 @@
 fun main() {
+    val adventOfCodeUtils = AdventOfCodeUtils()
     fun part1(input: List<String>): Int {
         return input.size
     }
@@ -11,11 +12,11 @@ fun main() {
     check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
+    val testInput = adventOfCodeUtils.readInput("Day01_test")
     check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = adventOfCodeUtils.readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,19 +3,19 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+class AdventOfCodeUtils {
+//    fun readInput(name: String) = this::class.java.classLoader.getResource(name).readText().trim().lines()
+    fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+}
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+    /**
+     * Converts string to md5 hash.
+     */
+    fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+    /**
+     * The cleaner shorthand for printing output.
+     */
+    fun Any?.println() = println(this)
