1c826bff10bcccca11304e0d8e128ecfcf74493c
juntingMa <junting.ma.e@thalesdigital.io>
2024-12-06 17:41:44 +0800

    chore: finish day 06

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,19 +3,16 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
-class AdventOfCodeUtils {
-//    fun readInput(name: String) = this::class.java.classLoader.getResource(name).readText().trim().lines()
-}
-    fun readInput(name: String) = Path("src/resources/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/resources/$name.txt").readText().trim().lines()
 
-    /**
-     * Converts string to md5 hash.
-     */
-    fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-        .toString(16)
-        .padStart(32, '0')
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
 
-    /**
-     * The cleaner shorthand for printing output.
-     */
-    fun Any?.println() = println(this)
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
